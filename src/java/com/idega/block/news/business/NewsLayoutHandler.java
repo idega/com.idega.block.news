38bb4139a625eb9c449e0819aa6dd5920f028708
Thorhallur Helgason <laddi@idega.com>
2007-07-18 16:13:05 +0000

    Fixed dubious code error created by someone who doesn't check hit Errors...

--- src/java/com/idega/block/news/business/NewsLayoutHandler.java
+++ src/java/com/idega/block/news/business/NewsLayoutHandler.java
@@ -1,5 +1,5 @@
 /*
- * $Id: NewsLayoutHandler.java,v 1.7 2007/06/21 10:17:58 valdas Exp $
+ * $Id: NewsLayoutHandler.java,v 1.8 2007/07/18 16:13:05 laddi Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -21,44 +21,45 @@ import com.idega.presentation.ui.DropdownMenu;
  * @version 1.0
  */
 public class NewsLayoutHandler implements ICPropertyHandler {
-  public static final int SINGLE_FILE_LAYOUT = 1;
-  public static final int NEWS_SITE_LAYOUT = 2;
-  public static final int NEWS_PAPER_LAYOUT = 3;
-  public static final int SINGLE_LINE_LAYOUT = 4;
-  public static final int COLLECTION_LAYOUT = 5;
 
-  /**
-   *
-   */
-  public NewsLayoutHandler() {
-  }
+	public static final int SINGLE_FILE_LAYOUT = 1;
+	public static final int NEWS_SITE_LAYOUT = 2;
+	public static final int NEWS_PAPER_LAYOUT = 3;
+	public static final int SINGLE_LINE_LAYOUT = 4;
+	public static final int COLLECTION_LAYOUT = 5;
 
-  /**
-   *
-   */
-  public List getDefaultHandlerTypes() {
-    return(null);
-  }
+	/**
+	 * 
+	 */
+	public NewsLayoutHandler() {
+	}
 
-  /**
-   *
-   */
-  public PresentationObject getHandlerObject(String name, String value, IWContext iwc, boolean oldGenerationHandler, String instanceId, String method) {
-    DropdownMenu menu = new DropdownMenu(name);
-    menu.addMenuElement("","Select:");
-    menu.addMenuElement(SINGLE_FILE_LAYOUT ,"SINGLEFILE");
-    menu.addMenuElement(NEWS_SITE_LAYOUT,"NEWSSITE");
-    menu.addMenuElement(NEWS_PAPER_LAYOUT ,"NEWSPAPER");
-    menu.addMenuElement(SINGLE_LINE_LAYOUT,"SINGLELINE");
-    menu.addMenuElement(COLLECTION_LAYOUT,"COLLECTION");
-    menu.setSelectedElement(value);
-    return(menu);
-  }
+	/**
+	 * 
+	 */
+	public List getDefaultHandlerTypes() {
+		return (null);
+	}
 
-  /**
-   *
-   */
-  public void onUpdate(String values[], IWContext iwc) {
-  }
+	/**
+	 * 
+	 */
+	public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
+		DropdownMenu menu = new DropdownMenu(name);
+		menu.addMenuElement("", "Select:");
+		menu.addMenuElement(SINGLE_FILE_LAYOUT, "SINGLEFILE");
+		menu.addMenuElement(NEWS_SITE_LAYOUT, "NEWSSITE");
+		menu.addMenuElement(NEWS_PAPER_LAYOUT, "NEWSPAPER");
+		menu.addMenuElement(SINGLE_LINE_LAYOUT, "SINGLELINE");
+		menu.addMenuElement(COLLECTION_LAYOUT, "COLLECTION");
+		menu.setSelectedElement(value);
+		return (menu);
+	}
+
+	/**
+	 * 
+	 */
+	public void onUpdate(String values[], IWContext iwc) {
+	}
 
 }
