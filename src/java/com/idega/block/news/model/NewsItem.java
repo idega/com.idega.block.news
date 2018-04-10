e5acf761560fe3ae8fc87f337cf4cf10a5ae3350
Valdas Žemaitis <valdas@idega.com>
2018-04-10 11:25:40 +0300

    New data structure

---
+++ src/java/com/idega/block/news/model/NewsItem.java
@@ -0,0 +1,99 @@
+package com.idega.block.news.model;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement
+@XmlType(name = "news")
+public class NewsItem implements Serializable {
+
+	private static final long serialVersionUID = -6816046379856388023L;
+
+	private int id;
+	private String title;
+	private String author;
+	private String teaser;
+	private String body;
+	private int categoryId;
+	private String category;
+	private String timestamp;
+	private String link;
+
+	@XmlAttribute(name = "id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTeaser() {
+		return teaser;
+	}
+
+	public void setTeaser(String teaser) {
+		this.teaser = teaser;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@XmlAttribute(name = "categoryId")
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+}
